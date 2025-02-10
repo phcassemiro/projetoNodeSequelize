1c8efa577f930db39e5dd6828e79a7832b3aa795
Pedro Henrique Cassemiro da Silva <phcassemiro@hotmail.com>
2025-02-10 12:00:50 -0300

    Criando o relacionamento entre tabelas

--- src/models/curso.js
+++ src/models/curso.js
@@ -4,13 +4,10 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Curso extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+      Curso.belongsTo(models.Categoria,{foreignkey: 'categoria_id'});
+      Curso.belongsTo(models.Pessoa,{foreignkey: 'docente_id'});
+      Curso.hasMany(models.Matricula,{foreignkey: 'curso_id'});
     }
   }
   Curso.init({
--- src/models/pessoa.js
+++ src/models/pessoa.js
@@ -4,13 +4,10 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Pessoa extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+
     static associate(models) {
-      // define association here
+      Pessoa.hasMany(models.Curso,{foreignkey: 'docente_id'});
+      Pessoa.hasMany(models.Matricula,{foreignkey: 'estudante_id'});
     }
   }
   Pessoa.init({
